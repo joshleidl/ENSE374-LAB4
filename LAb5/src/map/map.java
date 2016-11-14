949fbff5c579d98ced225bc8088d43ce8896d8d5
Konstantin Kharitonov <konstantinkharitoovpro@gmail.com>
2016-11-14 11:08:23 -0600

    Formatted the map file. Made the field array dynamic.

--- LAb5/src/map/map.java
+++ LAb5/src/map/map.java
@@ -6,22 +6,9 @@ public class map
 {
 	public static final char line		= '|';
 	public static final char blank 		= ' ';
-	public static final char dash		= '-';
-	private int max_rows 	= 10000;
-	private int max_columns = 10000;
 	private int row;
 	private int column;
-	private char[][] field;
-	
-	private int rowDifference(int input)
-	{
-		return (max_rows - input);
-	}
-	
-	private int columnDifference(int input)
-	{
-		return (max_columns - input);
-	}
+	private char field[][];
 	
 	public void setBorder()
 	{
@@ -46,10 +33,10 @@ public class map
 	
 	public void Clear()
 	{
-		field = new char[max_rows][max_columns];
-		for(int i = 0; i < (max_rows - rowDifference(row)); i++)
+		field = new char[row][column];
+		for(int i = 0; i < row; i++)
 		{
-			for (int j = 0; j < (max_columns - columnDifference(column)); j++)
+			for (int j = 0; j < column; j++)
 			{
 				field[i][j] = blank;
 			}
@@ -59,24 +46,21 @@ public class map
 	public final void Print()
 	{
 		int i = 0; int j = 0; int k = 0; int l = 0; int q = 0;
-		if ((column == 0) && (row == 0))
-		{
-			return;
-		}
+		if ((column == 0) && (row == 0)){ return; }
 		else
 		{
 			if ((i >= 0) || (j >= 0) || (k >= 0) || (l >= 0) || (q >= 0))
 			{
-				for(i = 0; i < (max_columns - columnDifference(column)) + 2; i++)
+				for(i = 0; i < column + 2; i++)
 				{
 					System.out.print(line);
 				}
 				System.out.print('\n');
 			
-				for (j = 0; j < (max_rows - rowDifference(row)); j++)
+				for (j = 0; j < row; j++)
 				{
 					System.out.print(line);
-					for(k = 0; k < (max_columns - columnDifference(column)); k++)
+					for(k = 0; k < column; k++)
 					{
 						System.out.print(field[j][k]);
 					
@@ -85,15 +69,13 @@ public class map
 					System.out.print('\n');
 				}
 			
-				for (l = 0; l < (max_columns - columnDifference(column)) + 2; l++)
+				for (l = 0; l < column + 2; l++)
 				{
 					System.out.print(line);
 				}
 				System.out.print('\n');
 			}
-		
-			else 
-				return;
+			else return;
 		}
 	}
 }
