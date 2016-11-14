2819489030ab9ce3d3cf3accee342b31ef991c89
Konstantin Kharitonov <konstantinkharitoovpro@gmail.com>
2016-11-13 22:38:25 -0600

    Now the map is complete. Added a few error checks.

--- LAb5/src/map/map.java
+++ LAb5/src/map/map.java
@@ -13,12 +13,12 @@ public class map
 	private int column;
 	private char[][] field;
 	
-	public int rowDifference(int input)
+	private int rowDifference(int input)
 	{
 		return (max_rows - input);
 	}
 	
-	public int columnDifference(int input)
+	private int columnDifference(int input)
 	{
 		return (max_columns - input);
 	}
@@ -26,12 +26,21 @@ public class map
 	public void setBorder()
 	{
 		Scanner in = new Scanner(System.in);
-		System.out.print("Rows: ");
-		int rowInput = in.nextInt();
-		System.out.print("Column: ");
-		int columnInput = in.nextInt();
-		row = rowInput;
-		column = columnInput;
+		
+		try
+		{
+			System.out.print("Rows: ");
+			int rowInput = in.nextInt();
+			System.out.print("Column: ");
+			int columnInput = in.nextInt();
+			row = rowInput;
+			column = columnInput;
+		}
+		
+		catch(InputMismatchException exception)
+		{
+			System.out.println("Invalid choice.");
+		}
 		in.close();
 	}
 	
@@ -50,35 +59,42 @@ public class map
 	public final void Print()
 	{
 		int i = 0; int j = 0; int k = 0; int l = 0; int q = 0;
-		if ((i >= 0) || (j >= 0) || (k >= 0) || (l >= 0) || (q >= 0))
+		if ((column == 0) && (row == 0))
 		{
-			
-			for(i = 0; i < (max_columns - columnDifference(column)) + 2; i++)
+			return;
+		}
+		else
+		{
+			if ((i >= 0) || (j >= 0) || (k >= 0) || (l >= 0) || (q >= 0))
 			{
-				System.out.print(line);
-			}
-			System.out.print('\n');
 			
-			for (j = 0; j < (max_rows - rowDifference(row)); j++)
-			{
-				System.out.print(line);
-				for(k = 0; k < (max_columns - columnDifference(column)); k++)
+				for(i = 0; i < (max_columns - columnDifference(column)) + 2; i++)
 				{
-					System.out.print(field[j][k]);
-					
+					System.out.print(line);
 				}
-				System.out.print(line);
+				System.out.print('\n');
+			
+				for (j = 0; j < (max_rows - rowDifference(row)); j++)
+				{
+					System.out.print(line);
+					for(k = 0; k < (max_columns - columnDifference(column)); k++)
+					{
+						System.out.print(field[j][k]);
+					
+					}
+					System.out.print(line);
+					System.out.print('\n');
+				}
+			
+				for (l = 0; l < (max_columns - columnDifference(column)) + 2; l++)
+				{
+					System.out.print(line);
+				}
 				System.out.print('\n');
 			}
-			
-			for (l = 0; l < (max_columns - columnDifference(column)) + 2; l++)
-			{
-				System.out.print(line);
-			}
-			System.out.print('\n');
-		}
 		
-		else 
-			return;
+			else 
+				return;
+		}
 	}
 }
